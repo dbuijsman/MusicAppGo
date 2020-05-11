b35bf88bc81584f76bf3b3cd4668f3954125d876
Dennis <dbuijsman@sogyo.nl>
2020-05-11 20:25:15 +0200

    Added FindArtistByID

--- discography/handlers/getInternal.go
+++ discography/handlers/getInternal.go
@@ -24,12 +24,43 @@ func (handler *MusicHandler) FindSongByID(response http.ResponseWriter, request 
 			general.SendError(response, http.StatusInternalServerError)
 			return
 		}
-		handler.Logger.Printf("Can't find song #v\n", songID)
+		handler.Logger.Printf("Can't find song #%v\n", songID)
+		general.SendError(response, http.StatusNotFound)
+		return
 	}
 	handler.Logger.Printf("Succesfully found song #%v: %v - %v\n", songID, song.Artists, song.Name)
 	response.Header().Set("Content-Type", "application/json")
 	response.WriteHeader(http.StatusOK)
 	err = general.WriteToJSON(&song, response)
+	if err != nil {
+		handler.Logger.Printf("[ERROR] %s\n", err)
+	}
+}
+
+// FindArtistByID returns the artist that belongs to the given ID.
+func (handler *MusicHandler) FindArtistByID(response http.ResponseWriter, request *http.Request) {
+	artistIDstring := mux.Vars(request)["id"]
+	artistID, err := strconv.Atoi(artistIDstring)
+	if err != nil {
+		handler.Logger.Printf("Received request with invalid id %v results in: %s\n", artistIDstring, err)
+		general.SendError(response, http.StatusBadRequest)
+		return
+	}
+	artist, searchErr := handler.db.FindArtistByID(artistID)
+	if searchErr != nil {
+		if searchErr.(general.DBError).ErrorCode != general.NotFoundError {
+			handler.Logger.Printf("[ERROR] Failed to search DB for artist #%v due to: %s\n", artistID, searchErr)
+			general.SendError(response, http.StatusInternalServerError)
+			return
+		}
+		handler.Logger.Printf("Can't find artist #%v\n", artistID)
+		general.SendError(response, http.StatusNotFound)
+		return
+	}
+	handler.Logger.Printf("Succesfully found artist #%v: %v\n", artistID, artist.Name)
+	response.Header().Set("Content-Type", "application/json")
+	response.WriteHeader(http.StatusOK)
+	err = general.WriteToJSON(&artist, response)
 	if err != nil {
 		handler.Logger.Printf("[ERROR] %s\n", err)
 	}
